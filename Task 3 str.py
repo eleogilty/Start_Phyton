3ee8abb6b9f41b017dc9d03fa352441b1d0d0b7c
eleogilty <leowwwdancer@gmail.com>
2025-04-15 15:33:57 +0300

    Add files via upload

---
+++ Task 3 str.py
@@ -0,0 +1,57 @@
+# # ЗАДАНИЕ 1
+# volume_disket = 1.44 * 1024 * 1024
+# page = 100
+# line = 50
+# symbol = 25
+# inf_symbol = 4
+# volume_book = page * line * symbol * inf_symbol
+# nun_book = volume_disket // volume_book
+# print(nun_book)
+# # ЗАДАНИЕ 2
+# # ЗАДАЧА
+# radius_of_a_circle = 5
+# side_of_the_square = 5
+# pi = const = 3.1415
+# area_of_a_circle = pi * radius_of_a_circle**2
+# print(round(area_of_a_circle, 2))
+# circumference = 2 * pi * radius_of_a_circle
+# print(round(circumference, 2))
+# perimeter_of_a_square = 4 * side_of_the_square
+# print(perimeter_of_a_square)
+# area_of_the_square = side_of_the_square**2
+# print(area_of_the_square)
+# #ЗАДАНИЕ 3
+# a = '0'
+# b = '1'
+# c = '2'
+# str_number = (a * 20) + (b * 50) + (c * 30)
+# print(str_number)
+# # ЗАДАНИЕ 4
+# coast = 0.125
+# speed = 4096
+# time = 120
+# free = 500
+# speed_in_kb_in_sec = speed / 1024
+# time_in_sec = time * 60
+# file = speed_in_kb_in_sec * time_in_sec
+# print(file)
+# money = (file - free) * coast
+# print(money)
+# # ЗАДАНИЕ 5
+# length = 90
+# width = 50
+# perimeter = (length + width) * 2
+# print(perimeter)
+#ЗАДАНИЕ 6
+# money = 10000
+# add = 5000
+# money_after_add = money + add
+# print(money_after_add)
+# Задание 7
+all_hours = 5000
+days = all_hours // 24
+print(days)
+hours = all_hours % days
+print(hours)
+
+
